2c84aae90c028d33386239623417f1c16a6f96c8
Peter Bull <peter.bull@ninesoftware.co.uk>
2022-11-16 19:25:15 +0000

    Start

---
+++ Code/Blazorfy/SpotifyProvider.cs
@@ -0,0 +1,38 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Spotify.NetStandard.Client;
+using Spotify.NetStandard.Client.Authentication;
+using Spotify.NetStandard.Client.Interfaces;
+using Spotify.NetStandard.Requests;
+using Spotify.NetStandard.Responses;
+
+namespace Blazorfy;
+
+// Provider Class
+public class SpotifyProvider
+{
+    // Constants
+
+    // Members
+
+    // Private Methods
+
+    // Constructor
+
+    // Property
+
+    // Login Method
+
+    // Logout Method
+
+    // Is Logged In Method
+
+    // Handle Code Method
+
+    // User Method
+
+    // List Method
+
+    // Search Method
+
+}
